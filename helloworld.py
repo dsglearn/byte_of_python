04282d8b2850bae672087d4ca20fb0568f7a997c
Dmitriy <dmitriy@Dmitriys-MacBook-Pro.local>
2023-01-21 22:08:18 +0500

    inital commit

---
+++ helloworld.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+print('What\'s ' 'your name?') # print is function
---
+++ str_format.py
@@ -0,0 +1,6 @@
+age = 26
+name = 'Swaroop'
+
+print('age {0} -- {1}'.format(name, age))
+
+print('{name} wrote {book}'.format(name='Swaroop', book='byte of python'))
